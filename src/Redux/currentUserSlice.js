5333da7cbecd85b90532b97d4cc6bb3b79ddb99a
Mohamed Gazya <mohamed.gazya.13@gmail.com>
2024-09-21 23:47:04 +0300

    Adding redux to project

---
+++ src/Redux/currentUserSlice.js
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const currentUserSlice = createSlice({
+    name: "currentUser",
+    initialState: {},
+    reducers: {}
+});
+
+export default currentUserSlice.reducer;
