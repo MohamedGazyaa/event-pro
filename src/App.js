9a16a31473a1e4ca1dcc4463d6c8f31ee6d4c671
Mohamed Gazya <mohamed.gazya.13@gmail.com>
2024-10-01 01:42:16 +0300

    setting vendor layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import VendorLayout from "./Vendor/VendorLayout";
+import Dashboard from "./Vendor/Pages/Dashboard";
+import Bookings from "./Vendor/Pages/Bookings";
+import Account from "./Vendor/Pages/Account";
+import Calendar from "./Vendor/Pages/Calendar";
+import { Services } from "./Vendor/Pages/Services";
 
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <Routes>
+        <Route path="/" element={<VendorLayout />}>
+          <Route path="/" element={<Dashboard />}/>
+          <Route path="/bookings" element={<Bookings />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/calendar" element={<Calendar />} />
+          <Route path="/services" element={<Services />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
